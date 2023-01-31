a1c1da3f5647fdce8fc1bbadb6490537c5371eb2
euclaise <jpritsk@purdue.edu>
2023-01-31 07:30:13 -0500

    Run files

--- main.go
+++ main.go
@@ -19,42 +19,55 @@ func validate(code string) bool {
 	return count == 0
 }
 
+func run(code string) {
+	p := NewParser(code)
+	
+	for len(p.data) > 0 {
+		v, err := p.GetValue()
+		p.skipWs()
+		if err != nil {
+			log.Fatalf("Erorr (parse): %s\n", err)
+		}
+
+		Top.ins = []Ins{}
+		if err := Gen(Top, v); err != nil {
+			log.Fatalf("Error (gen): %s\n", err)
+		}
+
+		Top.Eval()
+
+		if len(stack) > 0 {
+			PrintValue(stack[len(stack) - 1])
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
 	Top.scope = TopScope // Put builtins into top-level scope
 
-	reader := bufio.NewReader(os.Stdin)
+	switch len(os.Args) {
+	case 1:
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			fmt.Print("> ")
 
-	for {
-		fmt.Print("> ")
+			code, _ := reader.ReadString('\n')
 
-		code, _ := reader.ReadString('\n')
-
-		for !validate(code) {
-			fmt.Print(">> ")
-			next, _ := reader.ReadString('\n')
-			code += next
+			for !validate(code) {
+				fmt.Print(">> ")
+				next, _ := reader.ReadString('\n')
+				code += next
+			}
+			run(code)
 		}
-
-		p := NewParser(code)
-		
-		for len(p.data) > 0 {
-			v, err := p.GetValue()
-			p.skipWs()
-			if err != nil {
-				log.Fatalf("Erorr (parse): %s\n", err)
-			}
-
-			Top.ins = []Ins{}
-			if err := Gen(Top, v); err != nil {
-				log.Fatalf("Error (gen): %s\n", err)
-			}
-
-			Top.Eval()
-
-			if len(stack) > 0 {
-				PrintValue(stack[len(stack) - 1])
-			}
-			fmt.Println()
+	case 2:
+		b, err := os.ReadFile(os.Args[1])
+		if err != nil {
+			log.Fatalln("Error: Could not read file")
 		}
+		run(string(b))
+	default:
+		fmt.Printf("Usage: %s [filename]", os.Args[0])
 	}
 }
